b186dbe3c687f463935dea5366d092a554ed2f67
Fazil Abdulkhader <fazil.abdulkhader@revenuemed.com>
2013-08-02 14:50:23 +0530

    This is the initial commit to the branch

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="css/tab.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<script src="js/effects.js" type="text/javascript"></script>
+<script src="js/tabs_old.js"></script>
+<title>Live the Value</title>	
+	</head><meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<body>
+	<div class="overlay">
+		<div class="login_layout test">
+			<div class="login_logo"></div>
+				<div class="login_option">
+					<input type="button" class="login_btn" value="Sign Up" />
+						<div class="login_option_inner">
+						USERNAME<br />
+						<input type="text"  class="login_text_box logn " onfocus/><br /><br />
+						PASSWORD<br />
+						<input type="password"  class="login_text_box " /><br /><br />
+						
+						<input type="submit" value="Sign in" class="sign_in_btn" />  
+							<!--<img class="giff" src="images/ajax-loader (2).gif" />--><br /><br />
+						
+ 						<input type="checkbox"/><label style="margin-left:8px;">Remember Me</label><br /><br />
+						<a class="login_link" href="#">Forgot your password ?</a>
+<!--						<div class="error_box">
+						<label style="font-weight:bold">ERROR:</label> The password or username you entered <br />is incorrect. Lost your password?
+						</div>-->
+						</div>
+						<div class="login_copy_right"><a class="login_link" href="http://www.ruby-software.com/" target="_blank">© 2013 Ruby Software</a></div>
+				</div>
+	</div>
+	</body>
+</html>
