0c2698bc9ffe8812bdf6805a389c78408f32b38f
Fazil Abdulkhader <fazil.abdulkhader@revenuemed.com>
2013-08-30 16:34:35 +0530

    {addd new styles}

--- index.php
+++ index.php
@@ -1,36 +1,47 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<link rel="stylesheet" href="css/style.css" />
-<link rel="stylesheet" href="css/tab.css" />
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<script src="js/effects.js" type="text/javascript"></script>
-<script src="js/tabs_old.js"></script>
-<title>Live the Value</title>	
-	</head><meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<body>
-	<div class="overlay">
-		<div class="login_layout test">
-			<div class="login_logo"></div>
-				<div class="login_option">
-					<input type="button" class="login_btn" value="Sign Up" />
-						<div class="login_option_inner">
-						USERNAME<br />
-						<input type="text"  class="login_text_box logn " onfocus/><br /><br />
-						PASSWORD<br />
-						<input type="password"  class="login_text_box " /><br /><br />
-						
-						<input type="submit" value="Sign in" class="sign_in_btn" />  
-							<!--<img class="giff" src="images/ajax-loader (2).gif" />--><br /><br />
-						
- 						<input type="checkbox"/><label style="margin-left:8px;">Remember Me</label><br /><br />
-						<a class="login_link" href="#">Forgot your password ?</a>
-<!--						<div class="error_box">
-						<label style="font-weight:bold">ERROR:</label> The password or username you entered <br />is incorrect. Lost your password?
-						</div>-->
-						</div>
-						<div class="login_copy_right"><a class="login_link" href="http://www.ruby-software.com/" target="_blank">© 2013 Ruby Software</a></div>
-				</div>
+<title>Template</title>
+<link rel="stylesheet" href="css/global.css" />
+</head>
+<body>
+<div id="login_cont">
+<img src="images/activ.png" class="actv_img"/>
+	<form>
+	<input type="text" class="login_textbx"  name="username:" value="username:" onfocus="if(this.value == 'username:')
+	{ this.value = ''; }" onblur="if (this.value == '') { this.value='username:'; }" />
+	<input type="password" class="login_textbx" name="password:" value="password:" onfocus="if(this.value == 'password:')
+	{ this.value = ''; }" onblur="if (this.value == '') { this.value='password:'; }"/><br />
+	<div class="remb_me">
+	<input type="checkbox" />&nbsp;Remember me <img class="giff_ldr" src="images/ajax-loader.gif" /></div>
+	<label class="wrng_msg">Username or Password is Incorrect.</label>
+	<input type="submit" class="cmn_btn_small" value="LOGIN"/>
+	<label class="brd"></label>
+	<a href="#">Forgot Password</a>
+	</form>
+</div>
+	<div class="header">
+		<div class="header_includer">
+			<div class="logo">
+			<h1>SUCCESSDART</h1>
+			</div>
+			<nav>
+				<ul>
+					<li><a  class="cmn_btn_small" href="comptncy.html">Sign up </a></li>
+						<li><a href="#">Product</a></li>
+							<li><a href="#">features</a></li>
+								<li><a href="#">projects</a></li>
+					<li><a href="#" onclick="document.getElementById('login_cont').style.display='block'" >Sign in</a></li>
+				</ul>
+			</nav>
+			
+		</div>
 	</div>
-	</body>
+	<footer>
+	<div class="footr_lbl">
+Copyright © 2013<label class="yelw"> Ruby Software</label>
+	</div>
+</footer>
+</body>
 </html>
